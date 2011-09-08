77d8a2b2a8d320ac845bd86aa066f2ca0fd018b0
Ruth Ziegler <ziegler.ruth@gmail.com>
2011-09-08 22:21:28 +0200

    JavaDoc

--- wikiHow-app/src/ch/bbv/wikiHow/AboutActivity.java
+++ wikiHow-app/src/ch/bbv/wikiHow/AboutActivity.java
@@ -3,6 +3,12 @@ package ch.bbv.wikiHow;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * About view for settings.
+ * 
+ * @author ruthziegler
+ * 
+ */
 public class AboutActivity extends Activity {
 
 	@Override
